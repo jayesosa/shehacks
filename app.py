c842d982fa3d0a7bc7b7cf7f2d1690df65f6c111
Ruby Johnson <rubyj@umich.edu>
2018-01-27 05:57:50 -0500

    Back end dev
    Made a form
    Made an endpoint
    Used pickle to store

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
-from flask import Flask
+import pickle
+from flask import Flask, request
 app = Flask(__name__)
+
+#This makes an endpoint that the form will go to
+@app.route("/form", methods=["POST"])
+def form():
+	print (request.form)
+	values = request.form
+	singleRecord = Record(values['location'], values['age'], values['zipcode'],values['gender'], values['ethinicity'], values['height'], values['weight'], values['medication'],values['dosage'],values['dosing'], values['painAmount'], values['pillsUsed'])
+	records.append(singleRecord)
+	pickle.dump(records,open('records.pickle','wb'))
+	return ""
 
 @app.route("/")
 def hello():
@@ -12,5 +23,10 @@ def hello():
 @app.route("/goodbye")
 def goodbye():
 	return "goodbye"
+
+try:
+	records = pickle.load(open('records.pickle','rb'))
+except:
+	records = []
 
 app.run()
