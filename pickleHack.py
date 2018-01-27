c0779295b0f8f64f149ef2f6fb031d21a2f7cc3d
Ruby Johnson <rubyj@umich.edu>
2018-01-27 09:10:18 -0500

    pickleHack.py !!
    This should work

---
+++ pickleHack.py
@@ -0,0 +1,19 @@
+# IDK how to do this shit
+
+class Record
+	def __init__(self, location, age, zipcode, gender, ethinicity, height, weight, medication, dosage,dosing, painAmount, pillsUsed):
+
+			self.location = location
+			self.age = age
+			self.zipcode = zipcode
+			self.gender = gender
+			self.ethinicity = ethinicity
+			self.height = height
+			self.weight = weight
+			self.medication = medication
+			self.dosage = dosage
+			self.dosing = dosing
+			self.painAmount = painAmount
+			self.pillsUsed = pillsUsed
+
+
