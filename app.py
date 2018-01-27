b6fbf7188640605fea73f96a1e1f9c781c3a43ed
Jaye Sosa <Jaye@Jayes-MacBook-Pro.local>
2018-01-27 03:26:42 -0500

    update

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<b>Hello World!</b>"
+	html_file = open("structure.html")
+	html_response = html_file.read()
+	return html_response
+
 
 # this is tge goodbye thing
 @app.route("/goodbye")
